322c014f1df63763601f4bd0bdc0a004a2cd580c
Maksym <maksyutka4579@gmail.com>
2021-04-26 22:10:19 +0300

    fix sliders

--- js/script.js
+++ js/script.js
@@ -3,9 +3,8 @@ $(document).ready(function () {
         dots: true,
         responsive: [
             {
-                breakpoint: 768,
+                breakpoint: 576,
                 settings: {
-                    buttons: false,
                     arrows: false,
                 }
             }
@@ -16,5 +15,35 @@ $(document).ready(function () {
 $(document).ready(function () {
     $(".team__slider").slick({
         slidesToShow: 3,
+        responsive: [
+            {
+                breakpoint: 769,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    arrows: false,
+                    slidesToShow: 1,
+                }
+            }
+        ]
     });
+});
+
+const menu = document.querySelector('.menu-list');
+const burgerBtn = document.querySelector('.header__burger');
+
+burgerBtn.addEventListener("click", function (event) {
+    menu.classList.toggle("menu-list_disabled");
+});
+
+$("a.scroll-to").on("click", function (e) {
+    e.preventDefault();
+    const anchor = $(this).attr('href');
+    $('html, body').stop().animate({
+        scrollTop: $(anchor).offset().top - 60
+    }, 800);
 });
